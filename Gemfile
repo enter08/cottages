204dc50b9a82c94efe633b0e6790f647f4f6b070
enter08 <demicredzep1990@gmail.com>
2016-02-18 19:19:36 +0100

    Home page update and contact page

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'therubyracer'
 gem 'execjs'
 
+gem 'high_voltage'
+
 gem 'bootstrap-sass'
 
 # Use jquery as the JavaScript library
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
   get 'home/index'
-
-  # get 'main#index'
-
-  get 'home/index'
+  get 'about' => 'high_voltage/pages#show', id: 'about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
